c2754974694fa16edb0d909654d5dc982c927699
Tyler Croley <tjcroley@outlook.com>
2020-05-11 22:53:41 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,6 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :network 
   def actors_list
-    actors.collect {|actor| "#{actor.name}"}
+    actors.collect {|actor| "#{actor.first_name} #{actor.last_name}"}.join(" ")
   end
 end
